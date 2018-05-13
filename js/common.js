a27561888aa0c47910f68d13b8daa1081de50160
Your Name <you@example.com>
2018-05-13 23:55:36 +0800

    fixd

--- js/common.js
+++ js/common.js
@@ -5,6 +5,18 @@ $(document).ready(function(){
   $('.menuContent').delegate('.hasSub', 'click', function() {
     $(this).find('ul').toggle();
   });
+  function resetTab() {
+    $('.switchNav').find('span').each(function () {
+      $(this).removeClass('active');
+      $('.' + $(this).attr('name')).css('display', 'none');
+    });
+  }
+  $('.switchNav').delegate('span', 'click', function () {
+    resetTab();
+    $(this).addClass("active");
+    $('.' + $(this).attr('name')).css('display', 'block');
+  });
+
   $('.mobileMenuIcon').click(function () {
     $('.leftMenu').fadeIn(500);
     $('.menuMask').fadeIn(500);
